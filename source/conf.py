09d5954fca7c1c73fd324bf7c4a6210b26ff244f
Jack <jakitmationstudios@gmail.com>
2021-08-14 02:17:30 -0400

    Version Number

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Jackson Smith'
 author = 'Jackson Smith'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.0.5'
 
 
 # -- General configuration ---------------------------------------------------
